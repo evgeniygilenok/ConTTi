b59c2a93eb511327b0973eb8852dc60f9954f4a8
Eugene.Gil <mrluckydaemon@gmail.com>
2017-02-27 21:06:54 +0200

    task list
    add days to tasklist, change view

--- controllers/listctrl.js
+++ controllers/listctrl.js
@@ -21,18 +21,41 @@ var contti = angular.module('contti', []);
 		}
 		];
 
-		// $scope.priorities = ['very high', 'high', 'middle', 'low', 'very low'];
-		$scope.days = ['Inbox', 'Today', 'Tomorrow', 'Next week'];
+// priority data
+		$scope.priorities = [
+			{id: '1', name: 'very high'},
+			{id: '2', name: 'high'},
+			{id: '3', name: 'middle'},
+			{id: '4', name: 'low'},
+			{id: '5', name: 'very low'}
+			];
+		$scope.selectedPriority = {id: '3', name: 'middle'};
+
+// days data
+		$scope.days = [
+			{id: '1', name: 'Inbox'},
+			{id: '2', name: 'Today'},
+			{id: '3', name: 'Tomorrow'},
+			{id: '4', name: 'Next week'},
+		];
+		$scope.selectedDay = {id: '1', name: 'Inbox'};
 
 		$scope.addNew = function(){
-			// add logic for priority
-			$scope.taskList.push({todoText:$scope.enterTask, done:false, priority: $scope.selectedPriority});
+					$scope.taskList.push({
+						todoText:$scope.enterTask, 
+						done:false, 
+						priority: $scope.selectedPriority.name,
+						day: $scope.selectedDay.name
+					});
         	$scope.enterTask = "";
-        }
+        };
 		
 		$scope.remove = function(x){
 			$scope.taskList.splice(x, 1);
-		}
+		};
 
-		
+		$scope.addGroup = function(){
+			$scope.days.push({id: '5', name: 'new group'});	
+		};
 	});
+
