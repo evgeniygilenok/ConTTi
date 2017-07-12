e6b241c7fe2daed2ab84d7d89421836a505ee332
luckydaemon <evgeniygilenok@i.ua>
2017-07-12 20:50:28 +0300

    change bg

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
+var mongodb = require('mongodb');
 
-
-var app = express();
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('views engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
-
-
-
-var http = require('http');
-
-http.createServer(function(request, response) {
-	response.write("lala");
-	response.end();
-
-}).listen(1001);
+var uri = 'mongodb://localhost:27017/contti';
+mongodb.MongoClient.connect(uri, function(error, db) {
+	if (error) {
+		console.log(error);
+		process.exit(1);
+	}
+	db.collection('todos').find().toArray(function(error, docs) {
+		if (error) {
+			console.log(error);
+			process.exit(1);
+		}
+		console.log('Found docs');
+		docs.forEach(function(doc) {
+			console.log(JSON.stringify(doc));
+		});
+		process.exit(0);
+	});
+});
